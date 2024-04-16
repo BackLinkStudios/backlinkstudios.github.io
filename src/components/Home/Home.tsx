0f5980e9fcb905f6b023a6cbe6305dab4e536600
Arijit Kundu <arikundu9@gmail.com>
2024-04-16 12:51:55 +0530

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import "./Home.scss";
 export default function Home() {
     return (
         <>
-            <h2 className="h2_text">Get theTargeted andRelevant Traffic</h2>
+            <h2 className="h2_text">Get theTargeted andRelevant Traffic.</h2>
         </>
     );
 }
