e90086cd024e629a811383d05d3b59ef8d270dbf
Arijit Kundu <arikundu9@gmail.com>
2024-04-24 11:30:35 +0530

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
                     <div className="_._p-3 _._text-md md:_._text-lg _._font-semibold">email@domain.com</div>
                 </div>
             </div>
-            <div className="_._container _._mx-auto _._flex _._flex-wrap md:_._justify-between _._justify-center _._p-5">
+            <div className="_._mx-auto _._flex _._flex-wrap md:_._justify-between _._justify-center _._p-2 _._md:p-5">
                 <Link href="/" className="_._flex title-font _._font-medium _._items-center _._text-gray-900 _._mb-4">
                     <img src="/logo.png" alt="logo" width="200px" height="auto" />
                     {/* <div className="logo_text">
@@ -27,7 +27,7 @@ export default function Header() {
                 <div className="_._flex _._flex-wrap _._items-center _._text-base _._justify-center">
                     {NavLinks.map((nav, i) => {
                         return (
-                            <Link key={i} href={nav.path} className={`_._m-1.5 md:_._m-3 _._text-nowrap _._leading-none ${pathName === nav.path ? "_._text-[#6721ff]" : "_._text-white"} _._text-lg md:_._text-xl _._font-semibold hover:_._text-[#6721ff]`}>
+                            <Link key={i} href={nav.path} className={`_._m-1.5 md:_._m-3 _._text-nowrap _._leading-none ${pathName === nav.path ? "_._text-[#6721ff]" : "_._text-white"} _._text-sm md:_._text-xl _._font-semibold hover:_._text-[#6721ff]`}>
                                 <nav.icon style={{ display: "inline-block", marginTop: "-6px", marginRight: "2px" }} />
                                 {nav.name}
                             </Link>
