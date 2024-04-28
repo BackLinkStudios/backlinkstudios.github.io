8710519bb8907491f434bfae95212f46d5618b6b
Arijit Kundu <arikundu9@gmail.com>
2024-04-29 00:59:12 +0530

    lazy loading done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,43 @@
+"use client";
+import handleViewport, { type InjectedViewportProps } from "react-in-viewport";
 import "./styles.scss";
+
+const Stats = handleViewport(
+    (props: InjectedViewportProps<HTMLDivElement>) => {
+        const { inViewport, forwardedRef, enterCount } = props;
+        return (
+            <div ref={forwardedRef} className={`col-xs-12 col-sm-12 col-md-6 col-lg-6 counter-item-wrap-three ${inViewport && enterCount === 1 ? "animate__animated animate__fadeInRight animate__slow" : ""}`}>
+                <ul className="list-wrap">
+                    <li>
+                        <div className="counter-item-three">
+                            <h2 className="count">30+</h2>
+                            <p>Employees</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="counter-item-three">
+                            <h2 className="count">15K</h2>
+                            <p>Websites</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="counter-item-three">
+                            <h2 className="count">5K</h2>
+                            <p>Links Build</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="counter-item-three">
+                            <h2 className="count">300+</h2>
+                            <p>Clients Worldwide</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    } /**, options: {}, config: {} **/
+);
+
 export default function About() {
     return (
         <>
@@ -9,38 +48,11 @@ export default function About() {
             </div>
             <div className="container _._mx-auto _._px-5 _._mt-40">
                 <div className="row">
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 animate__animated animate__fadeInLeft animate__slow">
                         <h2 className="_._text-[34px] md:_._text-[38px]">Welcome to BacklinkStudios.com</h2>
                         <div className="_._text-lg/loose _._text-white/60">Here, we offer the best services that will ensure a boost to your ranking. We are digital specialists who help re-known brands push their brands beyond boundaries. We help our clients achieve this by ensuring best practices, such as using the best marketing techniques and data-driven insights and developing processes that unlock their business growth. We provide solutions and ideas for upgrading your business and taking it to the next level. We have the best project management software, digital marketing tools, and reporting tools that give them complete visibility over what we do and how it impacts their business. We love what we do, especially if it concerns pushing new technology. We have been in operation for a while and have created a bond with our clients. The number of our clients keeps growing daily. Currently, we have over 1000+ clients and over 150+ completed projects. We are passionate about creating the best strategies that help our customers reach their potential and push their products beyond borders.</div>
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 counter-item-wrap-three">
-                        <ul className="list-wrap">
-                            <li>
-                                <div className="counter-item-three">
-                                    <h2 className="count">30+</h2>
-                                    <p>Employees</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="counter-item-three">
-                                    <h2 className="count">15K</h2>
-                                    <p>Websites</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="counter-item-three">
-                                    <h2 className="count">5K</h2>
-                                    <p>Links Build</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="counter-item-three">
-                                    <h2 className="count">300+</h2>
-                                    <p>Clients Worldwide</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                    <Stats />
                 </div>
             </div>
         </>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import "animate.css";
 import "./Header.scss";
 import { usePathname } from "next/navigation";
 import { NavLinks } from "@/components/NavLinks";
@@ -18,20 +19,20 @@ export default function Header() {
             </div>
             <div className="container _._flex _._flex-wrap md:_._justify-between _._justify-center _._p-2 _._md:p-5">
                 <Link href="/" className="_._flex title-font _._font-medium _._items-center _._text-gray-900 _._mb-4">
-                    {/* <img src="/logo.png" alt="logo" width="200px" height="auto" /> */}
+                    <img src="/logo.png" alt="logo" width="200px" height="auto" />
                     {/* <div className="logo_text">
                         <span className="_._text-[#fe5855]">Back</span>
                         <span className="_._text-[#6721ff]">Link</span>Studio.com
                     </div> */}
-                    <span className="large">B</span>
+                    {/* <span className="large">B</span>
                     <span className="large S">S</span>
                     <span className="nor-text Backlink">Backlink</span>
-                    <span className="nor-text Studios">Studios</span>
+                    <span className="nor-text Studios">Studios</span> */}
                 </Link>
                 <div className="_._flex _._flex-wrap _._items-center _._text-base _._justify-center">
                     {NavLinks.map((nav, i) => {
                         return (
-                            <Link key={i} href={nav.path} className={`_._m-1.5 md:_._m-3 _._text-nowrap _._leading-none ${pathName === nav.path ? "active-link" : "_._text-white"} _._text-sm md:_._text-xl _._font-semibold hover:_._text-[#6721ff]`}>
+                            <Link key={i} href={nav.path} className={`_._m-1.5 _._pb-1 md:_._m-3 _._text-nowrap _._leading-none ${pathName === nav.path ? "active-link" : "_._text-white/90"} _._text-sm md:_._text-xl _._font-semibold hover:_._text-[#6721ff] animate__animated animate__backInDown animate__fast`}>
                                 <nav.icon style={{ display: "inline-block", marginTop: "-6px", marginRight: "2px" }} />
                                 {nav.name}
                             </Link>
