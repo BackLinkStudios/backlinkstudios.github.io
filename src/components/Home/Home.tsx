c848595805dda3e7087a02a4e2883ebbe243c957
Arijit Kundu <arikundu9@gmail.com>
2024-05-05 22:43:23 +0530

    home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,8 @@ import "./Home.scss";
 import BgSvg from "./BgSvg";
 import { FC, ReactElement } from "react";
 import Typer from "@/components/Typer/Typer";
+import { Container, Row, Col } from "react-bootstrap";
+import { Services } from "../ServiceList";
 // export default function Home() {
 //     return (
 //         <>
@@ -18,13 +20,46 @@ export const Home: FC<any> = (): ReactElement => {
         <>
             <div className="container _._mt-10">
                 <h2 className="h2_text _._text-center anim">Get the Targeted and Relevant Traffic.</h2>
-                <h2 className="h2_text _._text-start _._justify-start">
+                <h2 className="h2_text _._text-center">
                     <span className="gradient_text">
                         <Typer />
                     </span>
                 </h2>
             </div>
-            <BgSvg />
+            <br />
+            <br />
+            <br />
+            <Container>
+                <Row>
+                    <Col sm={12} md={6} lg={6}>
+                        <img src="./abt3.png" alt="" />
+                    </Col>
+                    <Col sm={12} md={6} lg={6}>
+                        <div className="_._p-2 _._flex _._align-middle _._justify-center">
+                            <div>
+                                <b>Welcome to The BacklinkStudios</b>
+                                <h1>Boost Your Ranking</h1>
+                                <p>At The link seller, we are not just about making profits. We aim to see our clients prosper and their business .Welcome to the link seller; here, we offer the best services that will ensure a boost to your ranking. We are digital specialists who help re-known brands push their brands beyond boundaries. We help our clients achieve this by ensuring best practices, such as using the best marketing techniques and data-driven insights and developing processes that unlock their business growth.</p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    {Services.map((service, i) => {
+                        return (
+                            <Col key={i} sm={12} md={6} lg={4}>
+                                <div className="box">
+                                    <h2>{service.heading}</h2>
+                                    <p>{service.body}</p>
+                                </div>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+            {/* <BgSvg /> */}
         </>
     );
 };
