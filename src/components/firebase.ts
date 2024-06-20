256a6cfe93d898ff3a9e9874a9ce2334143131aa
Arijit Kundu <arikundu9@gmail.com>
2024-06-20 17:44:53 +0530

    env setup, lets test

--- src/components/firebase.ts
+++ src/components/firebase.ts
@@ -3,12 +3,12 @@ import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDJ5-dEduwpvm8QD6sI0wf-HcYaqEgFSEQ",
-	authDomain: "blstudio.firebaseapp.com",
-	projectId: "blstudio",
-	storageBucket: "blstudio.appspot.com",
-	messagingSenderId: "740702309988",
-	appId: "1:740702309988:web:60952bb127ca0a14c2d971"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
