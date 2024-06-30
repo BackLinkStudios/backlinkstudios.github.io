d480109d8629cc1c372c89bcf26956a127081b43
Arijit Kundu <arikundu9@gmail.com>
2024-06-30 23:57:12 +0530

    32

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -17,55 +17,55 @@ import Link from "next/link";
 // }
 
 export const Home: FC<any> = (): ReactElement => {
-    return (
-        <>
-            <div className="container _._mt-10 animate__animated animate__flipInX animate__slow">
-                <h2 className="h2_text _._text-center anim">Get the Targeted and Relevant Traffic,</h2>
-                <h2 className="h2_text _._text-center">
-                    <span className="gradient_text">
-                        <Typer />
-                    </span>
-                </h2>
-            </div>
-            <br />
-            <br />
-            <br />
-            <Container className="_._mb-8">
-                <Row>
-                    <Col sm={12} md={6} lg={6}>
-                        <img src="./abt3.png" alt="" />
-                    </Col>
-                    <Col sm={12} md={6} lg={6}>
-                        <div className="_._p-2 _._flex _._align-middle _._justify-center _._items-center">
-                            <span>
-                                <b>Welcome to The Backlink Studios</b>
-                                <h1>Boost Your Ranking</h1>
-                                <p>At Backlink Studios, we are not just about making profits. We aim to see our clients prosper and their business .Welcome to the link seller; here, we offer the best services that will ensure a boost to your ranking. We are digital specialists who help re-known brands push their brands beyond boundaries. We help our clients achieve this by ensuring best practices, such as using the best marketing techniques and data-driven insights and developing processes that unlock their business growth.</p>
-                            </span>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            <Container>
-                <Row>
-                    {Services.map((service, i) => {
-                        return (
-                            <Col key={i} sm={12} md={6} lg={6}>
-                                <Link href={`/offered-services/${service.link}`} className="box _._flex _._flex-col md:_._flex-row _._text-white">
-                                    <div className="md:_._mr-5">
-                                        <img src={`/${service.icon}`} alt="" width="1000px" height="auto" />
-                                    </div>
-                                    <div>
-                                        <h3>{service.heading}</h3>
-                                        <p>{service.body}</p>
-                                    </div>
-                                </Link>
-                            </Col>
-                        );
-                    })}
-                </Row>
-            </Container>
-            {/* <BgSvg /> */}
-        </>
-    );
+	return (
+		<>
+			<div className="container _._mt-10 animate__animated animate__flipInX animate__slow">
+				<h2 className="h2_text _._text-center anim">Get the Targeted and Relevant Traffic,</h2>
+				<h2 className="h2_text _._text-center">
+					<span className="gradient_text">
+						<Typer />
+					</span>
+				</h2>
+			</div>
+			<br />
+			<br />
+			<br />
+			<Container className="_._mb-32">
+				<Row>
+					<Col sm={12} md={6} lg={6}>
+						<img src="./abt3.png" alt="" />
+					</Col>
+					<Col sm={12} md={6} lg={6}>
+						<div className="_._p-2 _._flex _._align-middle _._justify-center _._items-center">
+							<span>
+								<b>Welcome to The Backlink Studios</b>
+								<h1>Boost Your Ranking</h1>
+								<p>At Backlink Studios, we are not just about making profits. We aim to see our clients prosper and their business .Welcome to the link seller; here, we offer the best services that will ensure a boost to your ranking. We are digital specialists who help re-known brands push their brands beyond boundaries. We help our clients achieve this by ensuring best practices, such as using the best marketing techniques and data-driven insights and developing processes that unlock their business growth.</p>
+							</span>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+			<Container>
+				<Row>
+					{Services.map((service, i) => {
+						return (
+							<Col key={i} sm={12} md={6} lg={6}>
+								<Link href={`/offered-services/${service.link}`} className="box _._flex _._flex-col md:_._flex-row _._text-white">
+									<div className="md:_._mr-5">
+										<img src={`/${service.icon}`} alt="" width="1000px" height="auto" />
+									</div>
+									<div>
+										<h3>{service.heading}</h3>
+										<p>{service.body}</p>
+									</div>
+								</Link>
+							</Col>
+						);
+					})}
+				</Row>
+			</Container>
+			{/* <BgSvg /> */}
+		</>
+	);
 };
